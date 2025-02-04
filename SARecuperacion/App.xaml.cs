992c1810f75cbc15c7bed09f627e723d537f65d3
Bastian2704 <sebastian.abad@udla.edu.ec>
2025-02-04 09:39:09 -0500

    Agregar archivos de proyecto.

---
+++ SARecuperacion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SARecuperacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
